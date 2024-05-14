7a050a1fd1ddc3bb8380d09a9629df6c5fb5f37d
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2024-05-14 09:07:06 -0300

    HomeModel
    Created HomeModel structs

--- ChallengeiOS/Scenes/Home/HomeModel.swift
+++ ChallengeiOS/Scenes/Home/HomeModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Source: Decodable {
+    let id: String?
+    let name: String
+}
+
+struct Article: Decodable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String
+    let urlToImage: String?
+    let publishedAt: String
+    let content: String?
+}
+
+struct NewsResponse: Decodable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
